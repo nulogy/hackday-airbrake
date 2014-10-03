16a1e757b8756081d8c85e520ebe8882177839f6
Yazan Medanat <yazanm@nulogy.com>
2014-10-03 17:38:39 -0400

    WIP frontend bindings.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,23 +2,29 @@ define(['jquery', 'bootstrap', 'select2'], function ($) {
   $(".category").select2({
     placeholder: "Select a Category",
     allowClear: true,
-    // width: '200px',
     multiple: true,
-    data: {
-      results: [{
-        id: 'production',
-        text: 'production'
-      }, {
-        id: 'accounts',
-        text: 'accounts'
-      }]
+    ajax: {
+      url: function (term) {
+        return '/autocomplete/category/' + term;
+      }
+    },
+    results: function (data) {
+      return data;
     }
+    // data: {
+    //   results: [{
+    //     id: 'production',
+    //     text: 'production'
+    //   }, {
+    //     id: 'accounts',
+    //     text: 'accounts'
+    //   }]
+    // }
   });
 
   $(".environment").select2({
     placeholder: "Select an Environment",
     allowClear: true,
-    // width: '200px',
     multiple: true,
     data: {
       results: [{
@@ -29,5 +35,35 @@ define(['jquery', 'bootstrap', 'select2'], function ($) {
         text: 'demo'
       }]
     }
+  });
+
+  function serialize() {
+    return $("form").serialize();
+  }
+
+  function formatResults(results) {
+    var length = results.length;
+
+
+    $("form tbody").empty();
+
+    while (length--) {
+      var result = results[length],
+          tr = $("<tr />"),
+          controllerTd = $("<td />").text(result.controller),
+          categoryTd = $("<td />").text(result.category);
+
+      tr.append(controllerTd);
+      $("form tbody").append(tr);
+    }
+  }
+
+  $(".submit").click(function () {
+    $.ajax({
+      url: '/errors?' + serialize(),
+      success: function (results) {
+        formatResults(results);
+      }
+    })
   });
 });
